ee243d9a89851a7b46106b0302800d395d2f79f4
ankit-prajapati-24 <141832510+ankit-prajapati-24@users.noreply.github.com>
2024-01-19 22:09:03 +0530

    misttke solve

--- controllers/Payments.js
+++ controllers/Payments.js
@@ -27,32 +27,40 @@ exports.capturePayment = async(req,res) =>{
 }
 
 exports.verifyPayment = async(req, res) => {
+  console.log(req.body);
   const razorpay_order_id = req.body?.razorpay_order_id;
   const razorpay_payment_id = req.body?.razorpay_payment_id;
   const razorpay_signature = req.body?.razorpay_signature;
   const userId = req.body?.userId;
-
+   console.log("data fetched");
   if(!razorpay_order_id ||
       !razorpay_payment_id ||
       !razorpay_signature  || !userId) {
           return res.status(200).json({success:false, message:"Payment Failed"});
   }
 
+  console.log("data verify");
   let body = razorpay_order_id + "|" + razorpay_payment_id;
   const expectedSignature = crypto
       .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
       .update(body.toString())
       .digest("hex");
 
+      console.log("data created");
       if(expectedSignature === razorpay_signature) {
           //enroll karwao student ko
           // await enrollStudents(courses, userId, res);
           //return res
+          
+   console.log("data mathced");
            const user = await User.findOneAndUpdate({_id : userId},
              {
-              subscriptionToken: body.toString(),
-             }
+              SubsriptionToken: body.toString(),
+             },
+             {new:true}
             )
+            
+   console.log("user find");
           return res.status(200).json({success:true, message:"Payment Verified",user});
       }
       return res.status(200).json({success:"false", message:"Payment Failed"});
