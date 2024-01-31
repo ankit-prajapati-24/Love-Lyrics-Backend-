21334d01fc5e35deb2659198543574a2a77df3a5
ankit-prajapati-24 <141832510+ankit-prajapati-24@users.noreply.github.com>
2024-01-31 23:57:56 +0530

    add test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const http = require('http');
 const dbconnect = require("./config/connectToDb");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
@@ -11,8 +11,17 @@ const playlistRoutes = require("./routes/PlaylistRouts");
 const { ConnectCloadinary } = require("./config/Coudinary");
 const fileupload = require("express-fileupload");
 const dotenv = require("dotenv");
+const { Server } = require('socket.io');
+const server = http.createServer(app);
 dotenv.config(); // Load environment variables from .env file
 const PORT = process.env.PORT || 4000;
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    credentials:true,
+  }
+});
 
 // connect database
 dbconnect();
@@ -43,6 +52,29 @@ app.use("/api/v1/Album",albumsRouter);
 app.use("/api/v1/Auth",Authrouter);
 app.use("/api/v1/Playlist",playlistRoutes);
 
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+   
+  socket.on('chat-message', (msg,roomid,) => {
+      if(roomid != ""){
+      const allRooms = io.sockets.adapter.rooms;
+       socket.join(roomid);
+       console.log("all rooms",allRooms,allRooms.has(roomid));
+       socket.to(roomid).emit('chat-message', msg);
+      // socket.broadcast.to(roomid).emit('chat-message', msg);
+      }
+      else{
+          // socket.broadcast.emit('chat-message', msg);
+          socket.join("")
+      }
+      console.log('Received message:', msg,roomid);
+  });
+
+  socket.on('disconnect', () => {
+      console.log('User disconnected:', socket.id);
+  });
+});
 
 // default route
 app.get("/api", (req, res) => {
